18dfc9cc517e0ab4d7267eb43ccec2d7e5623678
Marco Chong <47628108+marco8chong@users.noreply.github.com>
2025-04-13 22:27:23 +0100

    Creating a Bullet Pool for the Turret

--- UnityCarDemo/Assets/CarDemo/Scripts/CarAddons/Turret/CarAddonTurret.cs
+++ UnityCarDemo/Assets/CarDemo/Scripts/CarAddons/Turret/CarAddonTurret.cs
@@ -1,4 +1,5 @@
 using NaughtyAttributes;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CarDemo
@@ -15,6 +16,29 @@ namespace CarDemo
         [Range(1.0f, 200.0f)]
         private float _firingForce = 50.0f;
 
+        [SerializeField]
+        [Range(10, 100)]
+        private int _bulletPoolSize = 30;
+
+        private Queue<CarAddonTurretBullet> _bulletPool = new Queue<CarAddonTurretBullet>();
+
+        private void Start()
+        {
+            CreateBulletPool();
+        }
+
+        private void CreateBulletPool()
+        {
+            for (int i = 0; i < _bulletPoolSize; i++)
+            {
+                CarAddonTurretBullet bullet = Instantiate<CarAddonTurretBullet>(_bullet);
+                bullet.transform.SetParent(_firePosition.transform, false);
+                bullet.ResetBullet();
+
+                _bulletPool.Enqueue(bullet);
+            }
+        }
+
         [Button]
         public override void TriggerAddon()
         {
@@ -22,12 +46,13 @@ namespace CarDemo
 
             if (_firePosition && _bullet)
             {
-                CarAddonTurretBullet bullet = Instantiate<CarAddonTurretBullet>(_bullet);
-                bullet.transform.SetParent(_firePosition.transform, false);
-                bullet.transform.position = _firePosition.transform.position;
-                bullet.transform.rotation = _firePosition.transform.rotation;
+                CarAddonTurretBullet bullet = _bulletPool.Dequeue();
 
+                bullet.ResetBullet();                
+                bullet.gameObject.SetActive(true);
                 bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * _firingForce, ForceMode.Impulse);
+            
+                _bulletPool.Enqueue(bullet);
             }
         }
     }
--- UnityCarDemo/Assets/CarDemo/Scripts/CarAddons/Turret/CarAddonTurretBullet.cs
+++ UnityCarDemo/Assets/CarDemo/Scripts/CarAddons/Turret/CarAddonTurretBullet.cs
@@ -8,7 +8,19 @@ namespace CarDemo
         [SerializeField]
         private float _lifespan = 5.0f;
 
+        private Rigidbody _rigidbody = null;
+
         private float _timeElapsed = 0.0f;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void OnEnable()
+        {
+            _timeElapsed = 0.0f;
+        }
 
         private void Update()
         {
@@ -16,8 +28,23 @@ namespace CarDemo
 
             if (_timeElapsed > _lifespan)
             {
-                GameObject.Destroy(gameObject);
+                _timeElapsed = 0.0f;
+
+                ResetBullet();
             }
+        }
+
+        public void ResetBullet()
+        {
+            _timeElapsed = 0.0f;
+
+            _rigidbody.linearVelocity = Vector3.zero;
+            _rigidbody.angularVelocity = Vector3.zero;
+
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+
+            gameObject.SetActive(false);
         }
     }
 }
